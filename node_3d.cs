4378258fb43f03e8b45ede18dd6ec4d8827d636d
OliBomby <olivierschipper@gmail.com>
2024-02-20 15:07:30 +0100

    add camera and c# script

---
+++ node_3d.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class node_3d : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Console.WriteLine("hello world");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
