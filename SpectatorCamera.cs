78dd7ffa352ac4394fe25ebd8d664f4016d4f1e8
OliBomby <olivierschipper@gmail.com>
2024-02-24 14:11:03 +0100

    added minecraft controls

--- SpectatorCamera.cs
+++ SpectatorCamera.cs
@@ -6,8 +6,8 @@ public partial class SpectatorCamera : Camera3D {
 	private Vector2 mouseDelta;
 	private float totalPitch;
 
-	[Export]
-	public float Sensitivity { get; set; } = 0.01f;
+	[Export] private float Sensitivity { get; set; } = 0.01f;
+	[Export] private float Speed { get; set; } = 10f;
 
 	public override void _Ready() {
 		Input.MouseMode = Input.MouseModeEnum.Captured;
@@ -24,15 +24,39 @@ public partial class SpectatorCamera : Camera3D {
 	}
 
 	public override void _Process(double delta) {
+		ProcessMouseLook();
+		ProcessMovement(delta);
+	}
+
+	private void ProcessMouseLook() {
+		if (Input.MouseMode != Input.MouseModeEnum.Captured)
+			return;
+
 		mouseDelta *= Sensitivity;
 		float yaw = mouseDelta.X;
 		float pitch = mouseDelta.Y;
 		mouseDelta = Vector2.Zero;
-		
+
 		pitch = Mathf.Clamp(pitch, -Mathf.Pi / 2 - totalPitch, Mathf.Pi / 2 - totalPitch);
 		totalPitch += pitch;
 
 		RotateY(-yaw);
 		RotateObjectLocal(new Vector3(1, 0, 0), -pitch);
+	}
+
+	private void ProcessMovement(double delta) {
+		var moveDir = new Vector3();
+		if (Input.IsActionPressed("move_forward")) moveDir += Vector3.Forward;
+		if (Input.IsActionPressed("move_backward")) moveDir += Vector3.Back;
+		if (Input.IsActionPressed("move_left")) moveDir += Vector3.Left;
+		if (Input.IsActionPressed("move_right")) moveDir += Vector3.Right;
+		if (Input.IsActionPressed("move_up")) moveDir += Vector3.Up;
+		if (Input.IsActionPressed("move_down")) moveDir += Vector3.Down;
+
+		var t = Transform;
+		moveDir = moveDir.Normalized() * (float)delta * Speed;
+		moveDir = moveDir.Rotated(Vector3.Up, Rotation.Y);
+		t.Origin += moveDir.Normalized() * (float)delta * Speed;
+		Transform = t;
 	}
 }
