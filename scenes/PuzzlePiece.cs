afc0273260a479de17ef1dd97a35bfead11bc611
OliBomby <olivierschipper@gmail.com>
2024-02-26 12:53:57 +0100

    add color to pieces

--- scenes/PuzzlePiece.cs
+++ scenes/PuzzlePiece.cs
@@ -3,17 +3,21 @@ using System;
 
 public partial class PuzzlePiece : Node3D
 {
-	Node Voxel;
+	MeshInstance3D Voxel;
 	public Vector3[] Pieces = {};
+
+	[Export]
+	public Color Color { get; set; }
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Voxel = GetNode("Voxel");
+		Voxel = GetNode<MeshInstance3D>("Voxel");
+		((StandardMaterial3D)Voxel.MaterialOverride).AlbedoColor = Color;
 	
 		
 		foreach (Vector3 VPos in Pieces) {
-			Node V = Voxel.Duplicate();
+			var V = Voxel.Duplicate();
 			AddChild(V);
 			(V as Node3D).Visible = true;
 			(V as Node3D).Position = VPos;
